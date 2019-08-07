54c109e4bc5b4119b1893fd191a4f1175a09075f
Yanik <syanik@yahoo.com>
2019-08-07 10:00:28 -0500

    Initial Commit

---
+++ UpfulTests/UpfulTests.swift
@@ -0,0 +1,34 @@
+//
+//  UpfulTests.swift
+//  UpfulTests
+//
+//  Created by Yanik Simpson on 8/7/19.
+//  Copyright © 2019 Yanik Simpson. All rights reserved.
+//
+
+import XCTest
+@testable import Upful
+
+class UpfulTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
