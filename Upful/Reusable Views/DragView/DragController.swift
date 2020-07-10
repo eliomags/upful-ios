8b7e0c7644cf274ff86260d4d941620f1024b3f7
Yanik <syanik@yahoo.com>
2020-07-09 23:37:33 -0500

    Create custom drag view

---
+++ Upful/Reusable Views/DragView/DragController.swift
@@ -0,0 +1,89 @@
+//
+//  DragController.swift
+//  Upful
+//
+//  Created by Yanik Simpson on 7/9/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+
+typealias DragStateConfiguration = DragController.DragStateConfiguration
+typealias DragViewPresentationState = DragController.DragViewPresentationState
+
+class DragController {
+    
+    struct DragStateConfiguration {
+        let closedHeight: CGFloat
+        let partialHeight: CGFloat
+        let fullHeight: CGFloat
+    }
+    
+    @objc enum DragViewPresentationState: Int, CaseIterable {
+        case closed
+        case partial
+        case full
+    }
+    
+    // MARK: Properties
+    
+    private(set) var configuration: DragStateConfiguration
+    
+    var currentPresentationState: DragViewPresentationState {
+        return DragViewPresentationState.allCases[currentPosition]
+    }
+    
+    var currentPosition: Int = DragViewPresentationState.closed.rawValue {
+        didSet {
+            presentationStateChangeHandler?(currentPresentationState)
+        }
+    }
+    
+    // MARK: Callbacks
+    
+    var presentationStateChangeHandler: ((DragViewPresentationState) -> Void)?
+    
+    // MARK: Initializer
+    
+    init(configuration: DragStateConfiguration) {
+        self.configuration = configuration
+    }
+    
+    // MARK: Methods
+    
+    static let velocityThreshold: CGFloat = 900
+    
+    func changeState(at velocity: CGFloat) {
+        let velocityDidMeetOrExceedThreshold = abs(velocity) >= DragController.velocityThreshold
+        
+        if velocity > 0 && velocityDidMeetOrExceedThreshold {
+            if currentPosition == DragViewPresentationState.allCases.count-1 { return }
+            currentPosition = DragViewPresentationState.allCases.count-1
+        }
+            
+        else if velocity < 0 && velocityDidMeetOrExceedThreshold {
+            if currentPosition == 0 { return }
+            currentPosition = 0
+        }
+    }
+    
+    func changeState(for position: CGFloat) {
+        let diffFromFull = abs(position - configuration.fullHeight)
+        let diffFromPartial = abs(position - configuration.partialHeight)
+        let diffFromClose = abs(position - configuration.closedHeight)
+        
+        let sortedDiffs = [abs(diffFromFull), abs(diffFromPartial), abs(diffFromClose)].sorted()
+        
+        if sortedDiffs.first! == diffFromFull {
+            currentPosition = DragViewPresentationState.full.rawValue
+        }
+        else if sortedDiffs.first! == diffFromPartial {
+            currentPosition = DragViewPresentationState.partial.rawValue
+        }
+        else if sortedDiffs.first! == diffFromClose {
+            currentPosition = DragViewPresentationState.closed.rawValue
+        }
+    }
+}
+
+
---
+++ Upful/Reusable Views/DragView/DragView.swift
@@ -0,0 +1,202 @@
+//
+//  DragView.swift
+//  Upful
+//
+//  Created by Yanik Simpson on 7/9/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol DragViewDelegate: class {
+    @objc optional func dragViewDidScroll(_ dragView: DragView)
+    @objc optional func dragViewDidBeginScrolling(_ dragView: DragView)
+    
+    func dragViewDidEndScrolling(_ dragView: DragView, dragState: DragViewPresentationState)
+}
+
+class DragView: UIView {
+    
+    // MARK: Properties
+    
+    var controller: DragController!
+    
+    weak var delegate: (DragViewDelegate & UITableViewDelegate & UITableViewDataSource)?
+    
+    // MARK: View
+    
+    private let dragIndicator: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 3
+        view.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.heightAnchor.constraint(equalToConstant: 6).isActive = true
+        view.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        return view
+    }()
+    
+    lazy var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        //        view.bounces = false
+        view.delegate = self
+        view.dataSource = self
+        view.isScrollEnabled = false
+        return view
+    }()
+    
+    // MARK: Gestures
+    
+    private lazy var defaultPanGesture: UIPanGestureRecognizer = {
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handleDefaultPan))
+        return gesture
+    }()
+    
+    // MARK: Initializer
+    
+    init(configuration: DragStateConfiguration) {
+        controller = DragController(configuration: configuration)
+        super.init(frame: .zero)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupView()
+    }
+    
+    private func commonInit() {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: controller.configuration.closedHeight).isActive = true
+        observePresentationStateChanges()
+    }
+    
+    private func observePresentationStateChanges() {
+        controller.presentationStateChangeHandler = { [unowned self] newState in
+            self.defaultPanGesture.cancel()
+            
+            let heightForState: CGFloat
+            switch self.controller.currentPresentationState {
+            case .closed:
+                heightForState = self.controller.configuration.closedHeight
+                self.tableView.isScrollEnabled = false
+                
+            case .partial:
+                heightForState = self.controller.configuration.partialHeight
+                self.tableView.isScrollEnabled = false
+                
+            case .full:
+                heightForState = self.controller.configuration.fullHeight
+                self.tableView.isScrollEnabled = true
+            }
+            
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: .curveLinear, animations: {
+                self.constraints.first { $0.firstAnchor == self.heightAnchor }?.isActive = false
+                self.heightAnchor.constraint(equalToConstant: heightForState).isActive = true
+            }) { _ in
+                self.delegate?.dragViewDidEndScrolling(self, dragState: newState)
+            }
+        }
+    }
+    
+    // MARK: View Setup
+    
+    func setupView() {
+        layer.cornerRadius = 16
+        backgroundColor = .white
+        
+        addGestureRecognizer(defaultPanGesture)
+        
+        addSubview(dragIndicator)
+        dragIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        dragIndicator.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: dragIndicator.bottomAnchor).isActive = true
+    }
+    
+    // MARK: Methods
+    
+    private var startPosition = CGPoint()
+    private var currentPosition = CGPoint()
+    
+    @objc private func handleDefaultPan(gesture: UIPanGestureRecognizer) {
+        guard gesture.translation(in: self).y != 0 else { return }
+        
+        let swipeVelocity = -gesture.velocity(in: self).y
+        
+        if gesture.state == .began {
+            startPosition = gesture.location(in: self)
+        }
+        
+        if gesture.state == .began || gesture.state == .changed {
+            currentPosition = gesture.location(in: self)
+            
+            if frame.height == controller.configuration.closedHeight && swipeVelocity < 0 {
+                fatalError()
+            }
+            
+            if frame.height == controller.configuration.fullHeight && swipeVelocity > 0 {
+                fatalError()
+            }
+            
+            //            controller.changeState(at: swipeVelocity)
+            
+            let difference = startPosition.y - currentPosition.y
+            handleDragHeightUpdate(newHeight: frame.height + difference)
+        }
+        
+        if gesture.state == .ended {
+            controller.changeState(for: bounds.height)
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if offset <= -25 {
+            controller.changeState(for: bounds.height-300)
+        }
+    }
+    
+    private func handleDragHeightUpdate(newHeight: CGFloat) {
+        self.constraints.first { $0.firstAnchor == self.heightAnchor }?.isActive = false
+        UIView.animate(withDuration: 0) {
+            self.heightAnchor.constraint(equalToConstant: newHeight).isActive = true
+        }
+    }
+}
+
+extension DragView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return delegate?.tableView(tableView, numberOfRowsInSection: section) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return delegate?.tableView(tableView, cellForRowAt: indexPath) ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let delegate = delegate else {
+            return UITableView.automaticDimension
+        }
+        return delegate.tableView!(tableView, heightForRowAt: indexPath)
+    }
+}
+
+extension UIPanGestureRecognizer {
+    
+    func cancel() {
+        isEnabled = false
+        isEnabled = true
+    }
+}
+
