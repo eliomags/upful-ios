8b13bf025c26c8067f3f0d5b6b8ba28a1e510eb7
Yanik <syanik@yahoo.com>
2020-08-07 19:54:12 -0500

    Create test cases for percentChangeCalculation expected results

--- UpfulTests/Features/Stock Detail/Overview/ViewModels/MetricPreviewViewModelTests.swift
+++ UpfulTests/Features/Stock Detail/Overview/ViewModels/MetricPreviewViewModelTests.swift
@@ -17,19 +17,12 @@ class MetricPreviewViewModelTests: XCTestCase {
         sut = MetricPreviewViewModel(ticker: "TEST", searchCriteria: .pricetoearnings)
     }
 
-    func testStateOnIntialization() {
+    func test_stateOnIntialization() {
         sut.fetchMetricData = Self.fetchEmptyResult
         
         XCTAssertFalse(sut.isLoading)
         XCTAssertTrue(sut.historicalData.isEmpty)
         XCTAssertFalse(sut.activityView.isAnimating)
-    }
-    
-    class DelegateTest: MetricPreviewViewModelDelegate {
-        var completion: (([CompanyHistoricalDatum]) -> Void)?
-        func didLoadCellData(cell: MetricPreviewTableViewCell?, with results: [CompanyHistoricalDatum]) {
-            completion?(results)
-        }
     }
     
     func test_loadingState() {
@@ -60,8 +53,55 @@ class MetricPreviewViewModelTests: XCTestCase {
         
         wait(for: [exp], timeout: 0.5)
     }
-
+    
+    func test_percentChangeCalculation_positiveToPositive() {
+        let percentChange = sut.calculateTotalChange(1, 2)
+        XCTAssertEqual(percentChange, "100%")
+    }
+    
+    func test_percentChangeCalculation_positiveToPositiveOverflow() {
+        let percentChange = sut.calculateTotalChange(1, 1000)
+        XCTAssertEqual(percentChange, "999%")
+    }
+    
+    func test_percentChangeCalculation_negativeToPositive() {
+        let percentChange = sut.calculateTotalChange(-1, 2)
+        XCTAssertEqual(percentChange, "999%")
+    }
+    
+    func test_percentChangeCalculation_negativeToNegative() {
+        let percentChange = sut.calculateTotalChange(-1.5, -3)
+        XCTAssertEqual(percentChange, "")
+    }
+    
+    func test_percentChangeCalculation_positiveToNegative() {
+        let percentChange = sut.calculateTotalChange(1, -3)
+        XCTAssertEqual(percentChange, "-999%")
+    }
+    
+    func test_percentChangeCalculation_zeroToPositive() {
+        let percentChange = sut.calculateTotalChange(0, 3)
+        XCTAssertEqual(percentChange, "999%")
+    }
+    
+    func test_percentChangeCalculation_zeroToNegative() {
+        let percentChange = sut.calculateTotalChange(0, 3)
+        XCTAssertEqual(percentChange, "")
+    }
+    
+    func test_percentChangeCalculation_positiveToZero() {
+        let percentChange = sut.calculateTotalChange(1, 0)
+        XCTAssertEqual(percentChange, "-100%")
+    }
+    
     // MARK: - Helper
+    
+    class DelegateTest: MetricPreviewViewModelDelegate {
+        var completion: (([CompanyHistoricalDatum]) -> Void)?
+        func didLoadCellData(cell: MetricPreviewTableViewCell?, with results: [CompanyHistoricalDatum]) {
+            completion?(results)
+        }
+    }
     
     private static func fetchEmptyResult(_ ticker: String, _ freq: FinancialsFrequency, _ criteria: SearchCriteria,
                                          _ completion: (Result<[CompanyHistoricalDatum], NetworkError>) -> Void) {
