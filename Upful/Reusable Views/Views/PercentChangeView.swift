2b3c51f73d7c9ef8b34205827ee2295960717983
Yanik <syanik@yahoo.com>
2020-09-23 17:00:10 -0500

    Remove ugly arrow images from quotes

--- Upful/Reusable Views/Views/PercentChangeView.swift
+++ Upful/Reusable Views/Views/PercentChangeView.swift
@@ -9,51 +9,30 @@
 import UIKit
 
 class PercentChangeView: UIView {
-    private let imageView: UIImageView = {
-        let im = UIImageView()
-        im.image = UIImage()
-        return im
-    }()
-    private lazy var imageBackground: UIView = {
-        let view = UIView()
-        view.backgroundColor = .clear
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(imageView)
-        imageView.fillSuperview(padding: .init(top: 2, left: 2, bottom: 2, right: 2))
-        return view
-    }()
     let percentChangeLabel: UILabel = {
         let label = UILabel()
         let size = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body).pointSize
         label.font = UIFont.systemFont(ofSize: size, weight: .semibold)
-        label.textAlignment = .center
+        label.textAlignment = .right
         return label
-    }()
-    private lazy var contentStackView: UIStackView = {
-        let sv = UIStackView(arrangedSubviews: [imageBackground, percentChangeLabel])
-        sv.axis = .horizontal
-        sv.spacing = 3
-        return sv
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(contentStackView)
-        contentStackView.fillSuperview()
+        addSubview(percentChangeLabel)
+        percentChangeLabel.fillSuperview()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func showNegative() {
-        imageView.image = UIImage(systemName: "arrow.down")?
-            .withTintColor(.systemRed, renderingMode: .alwaysOriginal)
+        percentChangeLabel.textColor = .systemRed
     }
     func showPositive() {
-        imageView.image = UIImage(systemName: "arrow.up")?
-            .withTintColor(.appAccent4, renderingMode: .alwaysOriginal)
+        percentChangeLabel.textColor = .systemGreen
     }
     func showNeutral() {
-        imageView.image = UIImage()
+        percentChangeLabel.textColor = .black
     }
 }
